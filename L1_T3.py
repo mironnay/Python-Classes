43a9a68aa53880822dcf4f7c34d876f4d1b6a447
mironnay <64540313+mironnay@users.noreply.github.com>
2020-04-29 17:06:01 +0300

    Add files via upload

---
+++ L1_T3.py
@@ -0,0 +1,4 @@
+person_list = []
+person_list.append(input("Enter your name: "))
+person_list.append(input("Enter your surname: "))
+print(person_list[0] + ' ' + person_list[1])
---
+++ L1_T4.py
@@ -0,0 +1,7 @@
+messages = []
+
+messages.append(input())
+messages.append(input())
+messages.append(input())
+
+print(messages[0] + ". "+ messages[1] + ". " + messages[2] + ".")
