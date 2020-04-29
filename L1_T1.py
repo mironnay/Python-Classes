0b259a8829d3d94ea801242b22584de4353bdb6b
mironnay <64540313+mironnay@users.noreply.github.com>
2020-04-29 18:33:09 +0300

    Delete L1_T1.py

--- L1_T1.py
+++
@@ -1,4 +0,0 @@
-num = input("Enter number of products you want to buy ")
-price = input("Enter price for each product ")
-final_price = int(num) * int(price)
-print("Final sum of products is: " + str(final_price))
