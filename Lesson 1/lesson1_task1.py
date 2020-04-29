9250b56e96d543cc2bb7e4417cc76407f837a72c
mironnay <64540313+mironnay@users.noreply.github.com>
2020-04-29 18:42:09 +0300

    Add files via upload

---
+++ Lesson 1/lesson1_task1.py
@@ -0,0 +1,4 @@
+num = input("Enter number of products you want to buy ")
+price = input("Enter price for each product ")
+final_price = int(num) * int(price)
+print("Final sum of products is: " + str(final_price))
---
+++ Lesson 1/lesson1_task4.py
@@ -0,0 +1,9 @@
+messages = []
+
+messages.append(input())
+messages.append(input())
+messages.append(input())
+
+print(messages[0], end =' ')
+print(messages[1], end =' ')
+print(messages[2], end =' ')
