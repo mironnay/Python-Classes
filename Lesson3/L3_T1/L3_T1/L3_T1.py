8a6311528a5dde091db2074313973536baf2771f
mironnay <64540313+mironnay@users.noreply.github.com>
2020-05-06 19:23:31 +0300

    Add files via upload

--- Lesson3/L3_T1/L3_T1/L3_T1.py
+++ Lesson3/L3_T1/L3_T1/L3_T1.py
@@ -1,20 +1,20 @@
 import random
 
-def fill_list(some_list = list(), list_start = 0, list_end = 100):
-    for i in range(list_start, list_end):
+def fill_list(some_list, list_lenth):
+    i = 0 
+    while i < list_lenth:
         some_list.append(random.randint(0, 100))
+        i += 1
 
-def print_pair_numbers(some_list = list()):
+def print_pair_numbers(some_list):
     print("List`s pair numbers: ")
-    i = 0
-    for i in range(0, len(some_list)):
-        if some_list[i] % 2 == 0:
+    for i in some_list:
+        if i % 2 == 0:
             print(some_list[i], end = ", ")
 
-list_lenth = 0
 my_list = list()
 
 list_lenth = int(input("Enter lenth of the list: "))
-fill_list(my_list, 0, list_lenth)
+fill_list(my_list, list_lenth)
 print("List: \n" + str(my_list))
 print_pair_numbers(my_list)
--- Lesson3/L3_T2/L3_T2/L3_T2.py
+++ Lesson3/L3_T2/L3_T2/L3_T2.py
@@ -1,17 +1,19 @@
 import random
 
-def fill_list(some_list = list(), list_start = 0, list_end = 100):
-    for i in range(list_start, list_end):
+def fill_list(some_list, list_lenth = 10):
+    i = 0 
+    while i < list_lenth:
         some_list.append(random.randint(0, 100))
+        i += 1
 
-def calc_list_summ(some_list = list()):
+def calc_list_summ(some_list ):
     sum = 0
-    for i in range(0, len(some_list)):
-        sum += some_list[i]
+    for i in some_list:
+        sum += i
     return sum
 
 my_list = list()
 
-fill_list(my_list, 0, 10)
+fill_list(my_list)
 print("Generated list: \n" + str(my_list))
 print("Sum: " + str(calc_list_summ(my_list)))
